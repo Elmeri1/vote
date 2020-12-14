19ea607c9c29009022a48ba49df66ac8b0c7021d
j0nttu123 <you@example.com>
2020-12-14 12:03:19 +0200

    db

--- functions.php
+++ functions.php
@@ -1,8 +1,8 @@
 <?php
 function pdo_connect_mysql() {
     // Update the details below with your MySQL details
-    require_once 'config/config.php'
-    
+    require_once 'config/config.php';
+
     try {
     	return new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PASS);
     } catch (PDOException $exception) {
